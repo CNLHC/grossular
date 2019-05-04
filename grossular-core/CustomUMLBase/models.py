b39e73067ff0fc3c03ae4a2e86e78abdcb002720
CNLHC <2463765697@qq.com>
2019-05-04 10:07:30 +0800

    add type model to grossular-core

--- grossular-core/CustomUMLBase/models.py
+++ grossular-core/CustomUMLBase/models.py
@@ -1,10 +1,16 @@
-
 from django.db import models
 from Project.models import GrossularProject
 from polymorphic.models import PolymorphicModel
 
+
 class GrossularCustomElement(PolymorphicModel):
-    grossularProject = models.ForeignKey(GrossularProject, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_related')
+    grossularProject = models.ForeignKey(GrossularProject, on_delete=models.CASCADE,
+                                         related_name='%(app_label)s_%(class)s_related')
 
     class Meta:
         abstract = True
+
+
+class GrossularCustomUMLType(GrossularCustomElement):
+    name = models.CharField(max_length=128)
+    comment = models.TextField()
